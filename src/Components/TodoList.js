222c3cc1d82cab1327adf554af2db29a29487fc0
eppalapallysharath@gmail.com <eppalapallysharath>
2023-02-18 21:40:39 +0530

    changes

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -32,7 +32,7 @@ const TodoList = ({
   };
   return (
     <div>
-      {todos.length > 0 ? <div>{todos.map((t, i) => (
+      {todos?.length > 0 ? <div>{todos.map((t, i) => (
         <li key={i} className="list-item">
           <input
             type="checkbox"
@@ -64,38 +64,6 @@ const TodoList = ({
       <button className="btn btn-danger my-3" onClick={clearAll}>
         Clear All
       </button></div>:<div className="text-center m-3">No todos present</div>}
-      {/* {todos.map((t, i) => (
-        <li key={i} className="list-item">
-          <input
-            type="checkbox"
-            checked={t.completed}
-            onChange={() => handleChecked(i)}
-          />
-          <span className={`list ${t.completed ? "complete" : ""}`}>
-            {t.text}
-          </span>
-
-          <div>
-            <button
-              type="button"
-              className="button-edit task-button"
-              onClick={() => handleEdit(i, t.text)}
-            >
-              <i className="fa fa-edit"></i>
-            </button>
-            <button
-              type="button"
-              onClick={() => handleDelete(i)}
-              className="button-delete task-button"
-            >
-              <i className="fa fa-trash"></i>
-            </button>
-          </div>
-        </li>
-      ))}
-      <button className="btn btn-danger my-3" onClick={clearAll}>
-        Clear All
-      </button> */}
     </div>
   );
 };
